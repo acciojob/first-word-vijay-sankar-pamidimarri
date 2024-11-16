a64ab761bf55ee972531039977405da9e852b9ba
Pamidimarri vijay sankar <sankerpamidimarri@gmail.com>
2024-11-16 13:32:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function firstWord(s) {
   // your code here
+	let arr=s.trim().split(" ");
+	if(arr.length==0){
+		return "";
+	}
+	return arr[0];
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
